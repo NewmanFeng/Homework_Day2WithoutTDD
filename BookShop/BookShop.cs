43c6a86712fad95073ee3f66c485653d26a7b8ad
NewmanFeng <charlsepheng@gmail.com>
2017-06-18 23:10:23 +0800

    init

---
+++ BookShop/BookShop.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BookShop
+{
+    public class BookShop
+    {
+        public decimal GetBillPrice(List<Book> books)
+        {
+            var count = books.Count(x => x.Count > 0);
+            var sum = 0.0m;
+            while (count > 0)
+            {
+                sum += books.Sum(x => x.SinglePrice * ((x.Count > 0) ? 1 : 0) * GetDiscount(count));
+                foreach (var book in books)
+                {
+                    book.Count = book.Count - 1;
+                }
+                count = books.Count(x => x.Count > 0);
+            }
+
+            return sum;
+        }
+
+        private decimal GetDiscount(int count)
+        {
+            var Discounts = new Dictionary<int, decimal>
+            {
+                {1, 1},
+                {2, 0.95m},
+                {3, 0.90m},
+                {4, 0.80m},
+                {5, 0.75m}
+            };
+            return Discounts[count];
+        }
+    }
+
+
+}
+public class Book
+{
+    public decimal SinglePrice { get; set; }
+    public string Name { get; set; }
+    public int SerialNo { get; set; }
+    public int Count { get; set; }
+}
---
+++ BookShopTests/BookShopTests.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookShop.Tests
+{
+    [TestClass()]
+    public class BookShopTests
+    {
+        [TestMethod()]
+        public void GetBillPriceTest1()
+        {
+            var books = new List<Book>()
+            {
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =1 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =2 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =3 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =4 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =5 ,SinglePrice = 100.0m},
+            };
+            var target=new BookShop();
+            var expected = 100.0m;
+
+            Assert.AreEqual(expected,target.GetBillPrice(books));
+        }
+        [TestMethod()]
+        public void GetBillPriceTest2()
+        {
+            var books = new List<Book>()
+            {
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =1 ,SinglePrice = 100.0m},
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =2 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =3 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =4 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =5 ,SinglePrice = 100.0m},
+            };
+            var target = new BookShop();
+            var expected = 190.0m;
+
+            Assert.AreEqual(expected, target.GetBillPrice(books));
+        }
+        [TestMethod()]
+        public void GetBillPriceTest3()
+        {
+            var books = new List<Book>()
+            {
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =1 ,SinglePrice = 100.0m},
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =2 ,SinglePrice = 100.0m},
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =3 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =4 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =5 ,SinglePrice = 100.0m},
+            };
+            var target = new BookShop();
+            var expected = 270.0m;
+
+            Assert.AreEqual(expected, target.GetBillPrice(books));
+        }
+        [TestMethod()]
+        public void GetBillPriceTest4()
+        {
+            var books = new List<Book>()
+            {
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =1 ,SinglePrice = 100.0m},
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =2 ,SinglePrice = 100.0m},
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =3 ,SinglePrice = 100.0m},
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =4 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =5 ,SinglePrice = 100.0m},
+            };
+            var target = new BookShop();
+            var expected = 320.0m;
+
+            Assert.AreEqual(expected, target.GetBillPrice(books));
+        }
+        [TestMethod()]
+        public void GetBillPriceTest5()
+        {
+            var books = new List<Book>()
+            {
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =1 ,SinglePrice = 100.0m},
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =2 ,SinglePrice = 100.0m},
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =3 ,SinglePrice = 100.0m},
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =4 ,SinglePrice = 100.0m},
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =5 ,SinglePrice = 100.0m},
+            };
+            var target = new BookShop();
+            var expected = 375.0m;
+
+            Assert.AreEqual(expected, target.GetBillPrice(books));
+        }
+        [TestMethod()]
+        public void GetBillPriceTest6()
+        {
+            var books = new List<Book>()
+            {
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =1 ,SinglePrice = 100.0m},
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =2 ,SinglePrice = 100.0m},
+                new Book() {Count = 2,Name = "HollyPoter",SerialNo =3 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =4 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =5 ,SinglePrice = 100.0m},
+            };
+            var target = new BookShop();
+            var expected = 370.0m;
+
+            Assert.AreEqual(expected, target.GetBillPrice(books));
+        }
+        [TestMethod()]
+        public void GetBillPriceTest7()
+        {
+            var books = new List<Book>()
+            {
+                new Book() {Count = 1,Name = "HollyPoter",SerialNo =1 ,SinglePrice = 100.0m},
+                new Book() {Count = 2,Name = "HollyPoter",SerialNo =2 ,SinglePrice = 100.0m},
+                new Book() {Count = 2,Name = "HollyPoter",SerialNo =3 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =4 ,SinglePrice = 100.0m},
+                new Book() {Count = 0,Name = "HollyPoter",SerialNo =5 ,SinglePrice = 100.0m},
+            };
+            var target = new BookShop();
+            var expected = 460.0m;
+
+            Assert.AreEqual(expected, target.GetBillPrice(books));
+        }
+    }
+}
